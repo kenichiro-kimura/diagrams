82eee4ca5d37fa3afa87a40faab597ac7ee48c0b
健一郎 木村 <k-kimura@alterbooth.com>
2020-09-02 14:20:25 +0900

    find packages

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setuptools.setup(
     long_description="",
     long_description_content_type="text/plain",
     url="https://github.com/kenichiro-kimura/",
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages("resources"),
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
