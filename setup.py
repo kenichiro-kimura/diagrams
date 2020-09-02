e300869030baf379c4abfda55862dd1bfcecacbf
健一郎 木村 <k-kimura@alterbooth.com>
2020-09-02 14:36:06 +0900

    add data_files

--- setup.py
+++ setup.py
@@ -10,6 +10,7 @@ setuptools.setup(
     long_description_content_type="text/plain",
     url="https://github.com/kenichiro-kimura/",
     packages=setuptools.find_packages(),
+    data_files=[('resources', ['resources/*.png','resources/*/*.png'])],
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
