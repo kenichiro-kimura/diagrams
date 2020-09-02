b839e2ffda6f373eb1d46a163dff891e8ed95d93
健一郎 木村 <k-kimura@alterbooth.com>
2020-09-02 13:39:09 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="diagrams",
+    version="1.0",
+    author="kkimura",
+    author_email="kimura@sokohiki.org",
+    description="patched diagrams",
+    long_description="",
+    long_description_content_type="text/plain",
+    url="https://github.com/kenichiro-kimura/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ]
+)
